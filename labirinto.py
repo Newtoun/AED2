93c787f51c86f198f734431712d617e38248a737
Nilton <nilton.nascimento@icomp.ufam.edu.br>
2023-02-16 17:34:32 -0400

    modifica labir

--- labirinto.py
+++ labirinto.py
@@ -3,9 +3,9 @@ from random import randint
 
 class labirinto:
 
-    def __init__(self,altura,largura):
-        self.altura = altura
-        self.largura = largura
+    def __init__(self,largura,altura):
+        self.largura = largura # X
+        self.altura = altura # Y
         self.matriz = self.inicializa_Matriz_Zero()
         self.ponto_Inicial = None
         self.ponto_Final = None
@@ -32,19 +32,26 @@ class labirinto:
 
         return lista
         
+
     def Gera_Labirinto(self):
         lista = []
+        altura = self.altura-2
+        largura = self.largura-2
+        x=0
+        y=0
 
-        y = randint(3,self.altura-2)
-        x = randint(3,self.largura-2)
+        print("largura {} altura {}".format(largura,altura))
+        while y%2==0 or x%2==0 and (y< altura and x< largura):
+            x = randint(3,(largura-1))
+            y = randint(3,(altura-1))
         print("Primeira escolha ({},{})".format(x, y))
-        self.matriz[x][y] = 1
+        self.matriz[x][y]=1
 
         lista = labirinto.retorna_Corredores(x,y)
         print("Lista: {}".format(lista))
-        #while len(lista) > 0:
-        cont = 1
-        for i in range(5):
+        while len(lista) > 0:
+            cont = 1
+        #for i in range(5):
             print("##LOOP {} ##".format(cont))
             escolha = randint(0,len(lista)-1)
             print("Escolha: {}".format(escolha))
@@ -53,9 +60,10 @@ class labirinto:
             lista.pop(escolha)
             print("Lista Sem: {}".format(lista))
             
-            if (Ponto[2] >= 0) and (Ponto[2] < self.largura-2) and (Ponto[3] >= 0) and (Ponto[3] < self.altura-2):
+            if (Ponto[2] >= 0) and (Ponto[2] < (largura)) and (Ponto[3] >= 0) and (Ponto[3] < (altura)):
                 print("passou no primeiro IF")
-                if (self.matriz[Ponto[2]][Ponto[3]]!=1):
+                print("({},{})".format(Ponto[2], Ponto[3]))
+                if (self.matriz[Ponto[2]][Ponto[3]]==0):
                     self.matriz[Ponto[0]][Ponto[1]] = 1
                     self.matriz[Ponto[2]][Ponto[3]] =  1
                     lista += labirinto.retorna_Corredores(Ponto[2],Ponto[3])
@@ -67,7 +75,7 @@ class labirinto:
 #matriz = labirinto.inicializa_Matriz_Zero(9,11)
 #labirinto.imprime_Matriz(matriz,9,11)
 
-mat = labirinto(9,11)
+mat = labirinto(11,9)
 mat.Gera_Labirinto()
 mat.imprime_Matriz()
  
--- teste.py
+++ teste.py
@@ -1,13 +1,9 @@
 from Ponto import Ponto
+from random import randint
+x = y = 0
 
-lista = set()
-vet = []
-a = Ponto(1,1)
-lista.add((a.x,a.y))
-print(lista)
-b = Ponto(2,1)
-c = Ponto(3,1)
-d = Ponto(1,1)
-print((c.x,c.y) in lista)
+while y%2==0 or x%2==0 and (y<9 and x<11):
+    x = randint(3,(11-2))
+    y = randint(3,(9-2))
 
-
+print("x {} Y{}".format(x,y))
