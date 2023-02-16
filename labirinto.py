87a1669cac028245dcbc817bc8e0e8de1a4a3f5a
Nilton <nilton.nascimento@icomp.ufam.edu.br>
2023-02-16 18:39:00 -0400

    labirinto corrigido doido

--- labirinto.py
+++ labirinto.py
@@ -23,17 +23,28 @@ class labirinto:
                 print(self.matriz[i][j],end=" ")
             print()
     
-    def retorna_Corredores(x,y):
+    def retorna_Corredores(lista_proibida,x,y):
         lista = []
-        lista.append((x-1,y,x-2,y))
-        lista.append((x,y-1,x,y-2))
-        lista.append((x+1,y,x+2,y))
-        lista.append((x,y+1,x,y+2))
+        if ((x-1,y,x-2,y)) not in lista_proibida:
+            lista.append((x-1,y,x-2,y))#esq
+            lista_proibida.add((x-1,y,x-2,y))
+        if ((x,y-1,x,y-2)) not in lista_proibida:    
+            lista.append((x,y-1,x,y-2))#cima
+            lista_proibida.add((x,y-1,x,y-2))
+        if ((x,y-1,x,y-2)) not in lista_proibida:
+            lista.append((x+1,y,x+2,y))#dir
+            lista_proibida.add((x+1,y,x+2,y))
+        if ((x,y+1,x,y+2)) not in lista_proibida:
+            lista.append((x,y+1,x,y+2))#baixo
+            lista_proibida.add((x,y+1,x,y+2))
+
 
         return lista
         
 
     def Gera_Labirinto(self):
+        matriz = labirinto.inicializa_Matriz_Zero(self)
+        lista_proibida = set()
         lista = []
         altura = self.altura-2
         largura = self.largura-2
@@ -45,9 +56,9 @@ class labirinto:
             x = randint(3,(largura-1))
             y = randint(3,(altura-1))
         print("Primeira escolha ({},{})".format(x, y))
-        self.matriz[x][y]=1
+        matriz[x][y]=1
 
-        lista = labirinto.retorna_Corredores(x,y)
+        lista = labirinto.retorna_Corredores(lista_proibida,x,y)
         print("Lista: {}".format(lista))
         while len(lista) > 0:
             cont = 1
@@ -60,15 +71,34 @@ class labirinto:
             lista.pop(escolha)
             print("Lista Sem: {}".format(lista))
             
-            if (Ponto[2] >= 0) and (Ponto[2] < (largura)) and (Ponto[3] >= 0) and (Ponto[3] < (altura)):
+            if (Ponto[2] >= 0) and (Ponto[2] <= (self.largura)) and (Ponto[3] >= 0) and (Ponto[3] <= (self.altura)):
                 print("passou no primeiro IF")
                 print("({},{})".format(Ponto[2], Ponto[3]))
-                if (self.matriz[Ponto[2]][Ponto[3]]==0):
-                    self.matriz[Ponto[0]][Ponto[1]] = 1
-                    self.matriz[Ponto[2]][Ponto[3]] =  1
-                    lista += labirinto.retorna_Corredores(Ponto[2],Ponto[3])
+                if (matriz[Ponto[2]][Ponto[3]]==0):
+                    print("Dentro Ponto = {}".format(matriz[Ponto[0]][Ponto[1]]))
+                    matriz[Ponto[0]][Ponto[1]] = 1
+                    matriz[Ponto[2]][Ponto[3]] =  1
+                    
+                    print()
+                    print("######################################")
+                    for i in range(self.altura):
+                        for j in range(self.largura):
+                            print(matriz[i][j],end=" ")
+                        print()
+                    print("######################################")
+                    print()
+                    lista += labirinto.retorna_Corredores(lista_proibida,Ponto[2],Ponto[3])
                     print("Lista ++: {}".format(lista))
+                else:
+                    print("valor matriz else {}".format(matriz[Ponto[2]][Ponto[3]]))
+                    print("valor não entra")
+            else:
+                print("NÂO ENTRA POR CONTA DO INTERVALO")
             cont += 1
+        for i in range(self.altura):
+            for j in range(self.largura):
+                print(matriz[i][j],end=" ")
+            print()
 
         
 
@@ -77,5 +107,5 @@ class labirinto:
 
 mat = labirinto(11,9)
 mat.Gera_Labirinto()
-mat.imprime_Matriz()
+#mat.imprime_Matriz()
  
